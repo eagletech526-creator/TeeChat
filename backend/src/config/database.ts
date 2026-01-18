6f6584613a405d90bc31a665b2f3406d7a6a5dd8
Fomekong Godwill <eagletech526@gmail.com>
2026-01-18 22:15:36 +0100

    Refactor database connection function to include environment variable check

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,12 +1,14 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-export const connectDB=async()=>{
-    try {
-        await mongoose.connect(process.env.MONGODB_URI as string)
-        console.log("MongoDB connected Succesfully ")
-    } catch (error) {
-        console.log("MongoDB connection Error", error);
-        process.exit(1) // exit with failure. 0 means success
-        
+export const connectDB = async () => {
+  try {
+    if (!process.env.MONGODB_URI) {
+      throw new Error("MONGODB_URI is not defined in environment variables");
     }
-}
+    await mongoose.connect(process.env.MONGODB_URI as string);
+    console.log("MongoDB connected Succesfully ");
+  } catch (error) {
+    console.log("MongoDB connection Error", error);
+    process.exit(1); // exit with failure. 0 means success
+  }
+};
